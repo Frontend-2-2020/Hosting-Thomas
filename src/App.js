b5fa4c28f4daf8b8bef3dbb211840eca158a0d2a
Thomas Velle <thomas.velle@gmail.com>
2020-03-30 19:43:25 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+        <h2> Thomas' Hosting Demo</h2>
+        <Slider {...settings}>
+          <div>
+            <img src="https://images.unsplash.com/photo-1585582857146-7886023ad3af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"></img>
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1585528063587-3ad4046b7254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"></img>
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1585457288697-180e55c12dbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"></img>{" "}
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1585582857146-7886023ad3af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"></img>{" "}
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1585582857146-7886023ad3af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"></img>{" "}
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1585582857146-7886023ad3af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"></img>{" "}
+          </div>
+        </Slider>
+      </div>
+    );
+  }
 }
 
 export default App;
